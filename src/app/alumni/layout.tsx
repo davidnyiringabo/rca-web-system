561fcb999de937ec935c875126bf8ea914416b55
davidnyiringabo <nyiringabodavid62@gmail.com>
2023-07-10 15:54:50 +0200

    all pages done

--- src/app/alumni/layout.tsx
+++ src/app/alumni/layout.tsx
@@ -13,7 +13,7 @@ export default function AboutUsLayout({
         <Navbar/>
         <Hero pageTitle={"Alumni Association"} heroContent={"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremquelaudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et Quasi architecto beatae vitae dicta sunt explicabo. Quis nostrud kemmith exercitation ullamco laboris nisi ut aliquip."}/>
         <div className="dynamic-content-container p-12 flex">
-            <Sidemenu parent={"alumni"} tabs={["contributions", "Alumni Admissions", "Living Styles After College", "Galley and Videos", "In The News", "Gallery and Videos", "Academic Affairs"]}/>
+            <Sidemenu parent={"alumni"} tabs={["contributions", "Alumni Admissions", "Living Styles After College", "Galley and Videos", "In The News"]}/>
             {children}
         </div>
         
