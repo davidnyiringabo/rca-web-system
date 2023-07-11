e3c34bfdaa65f2db7eef4a4b404dd3c9239f5d6c
davidnyiringabo <nyiringabodavid62@gmail.com>
2023-07-11 11:49:29 +0200

    keys problems solved

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -7,8 +7,8 @@ interface Props{
 }
 const Hero = ({pageTitle, heroContent}: Props)=>{
     return(
-        <div className="w-full p-24 flex justify-between items-center bg-[#D9D9D9] relative mt-[-3px]">
-            <div className="left-content">
+        <div className="hero w-full p-24 flex justify-between items-center bg-[#e3e2e2] relative mt-[-9px] relative">
+                <div className="left-content">
                 <h1 className="text-6xl font-extrabold text-white pb-10">{pageTitle}</h1>
                 <p className="w-1/2 leading-[2.5rem] text-[#4343436e]">{heroContent}</p>
             </div>
@@ -17,9 +17,6 @@ const Hero = ({pageTitle, heroContent}: Props)=>{
                 <Image src={playButtonImg} alt="" className="w-6 h-6"/>
             </div>
 
-            {/* <svg xmlns="http://www.w3.org/2000/svg" className="absolute w-full">
-                <path d="M1439.5 0.5H0.5V422L38 482.5L522 484.5L1439.5 382.5V0.5Z" fill="#d9d9d90a" stroke="black"/>
-            </svg> */}
         </div>
     )
 }
