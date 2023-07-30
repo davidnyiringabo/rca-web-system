dfda388351b5b62e59a76c38bfe9395a5e4a6456
MuhireIghor <muhireighor123@gmail.com>
2023-07-30 12:48:06 +0200

    [feat]Added the swiper navigation class

---
+++ src/utils/swiper.ts
@@ -0,0 +1,20 @@
+import { StaticImageData } from "next/image";
+
+export class SwiperNavigation {
+    items:StaticImageData[];
+    index:number;
+    constructor(items:StaticImageData[]){
+this.items = items;
+this.index=0
+    }
+    handleForward(){
+if(this.index <this.items.length ){
+    this.index++;
+}
+    }
+    handleBackward(){
+        if(this.index > 0){
+            this.index--;
+        }
+    }
+}
