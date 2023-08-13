591d7020c38199e9c0b3d78242e60e1f0f8c899e
davidnyiringabo <nyiringabodavid62@gmail.com>
2023-08-13 19:43:08 +0200

    error resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ import Card from "../components/card";
 import mission from '../assets/mission.png'
 import vission from '../assets/vision.png'
 import goals from '../assets/goal.png'
-import mainImg from '../assets/studentOne.jpg'
+import mainImg from '../assets/studentsOne.jpg'
 import img2 from '../assets/newThree.jpg'
 import Pathvec from '../assets/vectors/Pathvec'
 import { pathData } from "../data/adminPath";
